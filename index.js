653b1db2b6f90b3271bb3ed5122d8e9fea23c0be
Jose Santiago <josantorres93@gmail.com>
2020-02-29 09:10:32 -0600

    First commit: Express & Socket.io ready

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const socket = require('socket.io');
+
+const app = express();
+const server = app.listen(4000, () => {
+    console.log("now listening");
+});
+
+app.use(express.static('public'));
+
+const io = socket(server);
+io.on('connection', (socket) => {
+    console.log('socket connection', socket.id);
+})
